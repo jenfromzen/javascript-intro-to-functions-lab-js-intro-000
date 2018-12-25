e41553375cc8d75fb5fdd05862494ed467a33cdc
Jennifer Charney <jenfromzen@yahoo.com>
2018-12-25 03:57:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,16 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function
+function sayHiToGrandma(string) {
+  if (string === 'hello') {
+    return "I can't hear you!"
+  } else if
+  (string === 'HELLO') {
+    return "YES, INDEED!"
+  } else if
+  (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
+
+
